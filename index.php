b9a37b7a83273ea5d3805e0a0810fd068f80c5a9
jhon10101 <jhonzambrano21@gmail.com>
2023-07-17 16:34:38 -0400

    update change

--- index.php
+++ index.php
@@ -90,7 +90,7 @@
                                             <div class="mb-2">
 
                                             <!-- clear -->
-                                            <button type="button" value="clear" class="btn btn-lg btn-outline-warning btn-floating m-1 col-6 p-2 Btnclear">
+                                            <button type="button" value="clear" class="btn btn-lg btn-outline-warning btn-floating m-0 col-6 p-2 Btnclear">
                                             <i class="fa-1x">Limpiar</i></button>
 
                                             <!-- delete -->
