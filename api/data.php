70d8ea87466e282bab59e2b0af463f51238c9c3b
jhon10101 <jhonzambrano21@gmail.com>
2023-08-01 03:09:06 -0400

    update data

--- api/data.php
+++ api/data.php
@@ -1,14 +1,15 @@
 <?php
-$abc = $_POST["source"];
-if ("name" == $abc){
-  //  echo "bcv";
-}
+$source = $_POST["source"];
 
 $queryString = http_build_query([
     'access_key' => 'bdc0ad587af413b335abe28b50cd561a',
   ]);
   
-  $ch = curl_init(sprintf('%s?%s', 'https://apirestcalculator-production.up.railway.app/v1/sources/bcv.php', $queryString));
+    if ("Paralelo" == $source){
+        $ch = curl_init(sprintf('%s?%s', 'https://apirestcalculator-production.up.railway.app/v1/sources/paralelo.php', $queryString));
+    } else {
+        $ch = curl_init(sprintf('%s?%s', 'https://apirestcalculator-production.up.railway.app/v1/sources/bcv.php', $queryString));
+    }
 
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
                     
