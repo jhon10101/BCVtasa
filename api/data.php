d5e693e632a1de78ae762ec5bc765ab2dadcab62
jhon10101 <jhonzambrano21@gmail.com>
2024-05-06 19:28:22 -0400

    update

--- api/data.php
+++ api/data.php
@@ -1,21 +1,17 @@
 <?php
 $source = $_POST["source"];
 
-
-
 $queryString = http_build_query([
     'access_key' => 'bdc0ad587af413b335abe28b50cd561a',
   ]);
   
-    if ("BCV" == $source){
-        //$ch = curl_init(sprintf('%s?%s', 'https://apibcv.azurewebsites.net/v1/sources/bcv.php', $queryString));
-          $ch = curl_init(sprintf('%s?%s', 'https://prime-edita-jhon10101.koyeb.app/v1/sources/bcv.php', $queryString));
-      } elseif ("PDVSA" == $source) {
-          $ch = curl_init(sprintf('%s?%s', 'http://localhost/PHP/ApiRestCalculator/v1/sources/pdvsa.php/v1/sources/paralelo.php', $queryString));
-      } else {
-        $ch = curl_init(sprintf('%s?%s', 'https://prime-edita-jhon10101.koyeb.app/v1/sources/paralelo.php', $queryString));
-      //$ch = curl_init(sprintf('%s?%s', 'https://apibcv.azurewebsites.net/v1/sources/paralelo.php', $queryString));
-    }
+  if ("BCV" == $source){
+        $ch = curl_init(sprintf('%s?%s', 'https://forward-deena-jhonny91.koyeb.app/v1/sources/bcv.php', $queryString));
+    } elseif ("PDVSA" === $source) {
+        $ch = curl_init(sprintf('%s?%s', 'https://forward-deena-jhonny91.koyeb.app/v1/sources/pdvsa.php', $queryString));
+    } else {
+      $ch = curl_init(sprintf('%s?%s', 'https://forward-deena-jhonny91.koyeb.app/v1/sources/paralelo.php', $queryString));
+  }
 
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
                     
